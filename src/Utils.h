2e9a6ba7a3a17b0f762c900bddcc7b93c5369312
Phan Gia Huy <phangiahuy290407@gmail.com>
2026-06-02 09:47:02 +0700

    Sửa file Utils

---
+++ src/Utils.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//Window
+const int Win_W = 1920;
+const int Win_H = 1080;
+const int FPS = 60;
+
+//Game
+const int Max_Level = 5;
+const int Num_Lanes = 6;
+const int Lane_H = 80;
+const int Safe_Zone_H = 80;
+
+//Player
+const float Player_Speed = 5;
+const float Player_W = 48.f;
+const float Player_H = 64.f;
+
+//Animation
+const float Frame_Time = 0.12f;
+
+//File path
+const char Save_Path[] = "saves/save.txt";
+const char Score_Path[] = "saves/highscore.txt";
+const char Front_Path[] = "assets/front/font.ttf";
