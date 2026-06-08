e9ac45e9fa63737cb17f355441e2d0126aa03d26
Phan Gia Huy <phangiahuy290407@gmail.com>
2026-06-08 17:06:36 +0700

    Remove IDE and builder folder

--- build/CMakeFiles/CrossingGame.dir/src/core/CTRAFFICLIGHT.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CrossingGame.dir/src/core/CTRAFFICLIGHT.cpp.o: \
-  /Users/phangiahuy290407gmail.com/Documents/bài\ tập/file\ pdf/bài\ tập/hk3/oop/Crossing\ Street/src/core/CTRAFFICLIGHT.cpp
--- build/CMakeFiles/CrossingGame.dir/src/obstacles/lv4/CVEHICLE.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CrossingGame.dir/src/obstacles/lv4/CVEHICLE.cpp.o: \
-  /Users/phangiahuy290407gmail.com/Documents/bài\ tập/file\ pdf/bài\ tập/hk3/oop/Crossing\ Street/src/obstacles/lv4/CVEHICLE.cpp
