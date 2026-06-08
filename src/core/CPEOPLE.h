bd88139426bbbeb22f5cddb0c0ed3fa76408a04b
NguyenHoangNhat-ProMax <nhnhat2529@clc.fitus.edu.vn>
2026-06-08 20:18:02 +0700

    Task day1 D

---
+++ src/core/CPEOPLE.h
@@ -0,0 +1,37 @@
+#ifndef CROSSINGGAME_CPEOPLE_H
+#define CROSSINGGAME_CPEOPLE_H
+
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "Utils.h"
+
+class CPEOPLE
+{
+    sf::Sprite mSprite;
+    sf::Texture mTexture;
+
+    sf::Vector2f mPosition;
+
+    float mSpeed;
+    bool mIsDead;
+    bool mIsFinish;
+
+public:
+    CPEOPLE();
+
+    void setPosition(float x, float y);
+    sf::Vector2f getPosition() const;
+
+    sf::FloatRect getBounds() const;
+
+    bool isDead() const;
+    bool isFinish() const;
+
+    void setDead(bool dead);
+    void setFinish(bool finish);
+
+    void draw(sf::RenderWindow& window);
+};
+
+#endif // CROSSINGGAME_CPEOPLE_H
