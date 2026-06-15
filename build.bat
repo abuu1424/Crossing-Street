35a33c0b44b1e8f7b86dea141092b410df9a8f1c
Phan Gia Huy <phangiahuy290407@gmail.com>
2026-06-15 20:14:25 +0700

    Build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ set PROJECT_NAME=CrossingGame
 set BUILD_DIR=build_win
 set SFML_DIR=C:\SFML-2.6.2
 
-:: Clean
 if "%1"=="clean" (
     if exist "%BUILD_DIR%" (
         echo [INFO] Removing build directory...
@@ -13,46 +12,32 @@ if "%1"=="clean" (
     )
 )
 
-:: Create build directory
-if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
-cd "%BUILD_DIR%"
-
-:: Configure
-cmake .. ^
-    -G "Visual Studio 18 2026" ^
+cmake -S . -B "%BUILD_DIR%" ^
     -A x64 ^
     -DCMAKE_PREFIX_PATH="%SFML_DIR%"
 
 if errorlevel 1 (
     echo.
     echo [ERROR] CMake configure failed!
-    cd ..
     pause
     exit /b 1
 )
 
-:: Build
-cmake --build . --config Release
+cmake --build "%BUILD_DIR%" --config Release
 
 if errorlevel 1 (
     echo.
     echo [ERROR] Build failed!
-    cd ..
     pause
     exit /b 1
 )
 
-:: Copy SFML DLLs
 echo [INFO] Copying SFML DLLs...
-copy "%SFML_DIR%\bin\*.dll" "Release\" /Y >nul
-
-:: Back to project root
-cd ..
+copy "%SFML_DIR%\bin\*.dll" "%BUILD_DIR%\Release\" /Y >nul
 
 echo.
 echo [OK] Build successful!
 
-:: Run
 if "%1"=="run" goto :run
 if "%2"=="run" goto :run
 goto :end
