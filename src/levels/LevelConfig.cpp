99cfda96dca3da804aa6750f54af77f8d20d5e4f
Phan Gia Huy <phangiahuy290407@gmail.com>
2026-06-20 17:30:51 +0700

    THÊM CON TRÁI

--- src/levels/LevelConfig.cpp
+++ src/levels/LevelConfig.cpp
@@ -11,14 +11,14 @@ LevelConfig getLevel1() {
         {
 
                 { ObstacleType::DINOSAUR, 150.f,  1.f, 220.f, "assets/sprites/lv1_sp/dino/dino1.png", 3, 420.f },
-                { ObstacleType::DINOSAUR, 200.f, -1.f, 315.f, "assets/sprites/lv1_sp/dino/dino2.png", 3, 430.f },
+                { ObstacleType::DINOSAUR, 200.f, -1.f, 315.f, "assets/sprites/lv1_sp/dino/dino2_trai.png", 3, 430.f },
                 { ObstacleType::MAMMOTH, 90.f, 1.f, 405.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f },
-                { ObstacleType::DINOSAUR, 120.f, -1.f, 500.f, "assets/sprites/lv1_sp/dino/dino3.png", 3, 430.f },
+                { ObstacleType::DINOSAUR, 120.f, -1.f, 500.f, "assets/sprites/lv1_sp/dino/dino3_trai.png", 3, 430.f },
                 { ObstacleType::MAMMOTH, 80.f, 1.f, 80.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f },
             },
             {
                 { AnimalType::BIRD, 150.f,  1.f, 115.f, "assets/sprites/lv1_sp/bird/bay.png", 2, 520.f },
-                { AnimalType::BIRD, 200.f, -1.f, 115.f, "assets/sprites/lv1_sp/bird/bay.png", 2, 520.f },
+                { AnimalType::BIRD, 200.f, -1.f, 115.f, "assets/sprites/lv1_sp/bird/baytrai.png", 2, 520.f },
             }
     };
 }
