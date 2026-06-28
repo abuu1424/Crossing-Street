3c28109aace3a215f3432a6fae93eb8dde95c538
Phan Gia Huy <phangiahuy290407@gmail.com>
2026-06-28 09:22:21 +0700

    ẢNH hud

--- src/levels/LevelConfig.cpp
+++ src/levels/LevelConfig.cpp
@@ -1,6 +1,7 @@
 #include "LevelConfig.h"
 
-LevelConfig getLevel1() {
+LevelConfig getLevel1()
+{
     return {
         1,
         "assets/background/lv1_bg/bg_lv1.png",
@@ -9,20 +10,20 @@ LevelConfig getLevel1() {
         "assets/ui/hud/tiensu.png",
         "assets/sprites/lv1_sp/traffic/torch.png",
         "assets/sprites/lv1_sp/traffic/torch1.png",
-        80, 550,
+        80,
+        550,
         {
 
-                { ObstacleType::DINOSAUR, 150.f,  1.f, 220.f, "assets/sprites/lv1_sp/dino/dino1.png", 3, 420.f },
-                { ObstacleType::DINOSAUR, 200.f, -1.f, 315.f, "assets/sprites/lv1_sp/dino/dino2_trai.png", 3, 430.f },
-                { ObstacleType::MAMMOTH, 90.f, 1.f, 405.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f },
-                { ObstacleType::DINOSAUR, 120.f, -1.f, 500.f, "assets/sprites/lv1_sp/dino/dino3_trai.png", 3, 430.f },
-                { ObstacleType::MAMMOTH, 80.f, 1.f, 80.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f },
-            },
-            {
-                { AnimalType::BIRD, 150.f,  1.f, 115.f, "assets/sprites/lv1_sp/bird/bay.png", 2, 520.f },
-                { AnimalType::BIRD, 200.f, -1.f, 115.f, "assets/sprites/lv1_sp/bird/baytrai.png", 2, 520.f },
-            }
-    };
+            {ObstacleType::DINOSAUR, 150.f, 1.f, 220.f, "assets/sprites/lv1_sp/dino/dino1.png", 3, 420.f},
+            {ObstacleType::DINOSAUR, 200.f, -1.f, 315.f, "assets/sprites/lv1_sp/dino/dino2_trai.png", 3, 430.f},
+            {ObstacleType::MAMMOTH, 90.f, 1.f, 405.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f},
+            {ObstacleType::DINOSAUR, 120.f, -1.f, 500.f, "assets/sprites/lv1_sp/dino/dino3_trai.png", 3, 430.f},
+            {ObstacleType::MAMMOTH, 80.f, 1.f, 80.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f},
+        },
+        {
+            {AnimalType::BIRD, 150.f, 1.f, 115.f, "assets/sprites/lv1_sp/bird/bay.png", 2, 520.f},
+            {AnimalType::BIRD, 200.f, -1.f, 115.f, "assets/sprites/lv1_sp/bird/baytrai.png", 2, 520.f},
+        }};
 }
 
 LevelConfig getLevel2()
@@ -32,20 +33,19 @@ LevelConfig getLevel2()
         "assets/background/lv2_bg/bg_lv2.png",
         "assets/sounds/lv2_sd/music_bg2.ogg",
         "assets/sprites/lv2_sp/player/player.png",
-        "assets/ui/hud/cổ_đại.png",
+        "assets/ui/hud/codai.png",
         "assets/sprites/lv2_sp/traffic/torch.png",
         "assets/sprites/lv2_sp/traffic/torch1.png",
-        80, 550,
-        {
-        },
-        {
-                {AnimalType::EAGLE, 180.f, 1, 115.f, "assets/sprites/lv2_sp/bird/kenkenaicap.png", 2, 520.f}
-        }
-    };
+        80,
+        550,
+        {},
+        {{AnimalType::EAGLE, 180.f, 1, 115.f, "assets/sprites/lv2_sp/bird/kenkenaicap.png", 2, 520.f}}};
 };
 
-LevelConfig getLevel(int level) {
-    switch (level) {
+LevelConfig getLevel(int level)
+{
+    switch (level)
+    {
     case 1:
         return getLevel1();
     case 2:
