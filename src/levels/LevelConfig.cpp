b3635ca18c35451a9b1816479103f99860d2c948
Phan Gia Huy <phangiahuy290407@gmail.com>
2026-06-30 09:45:50 +0700

    THAY ĐỔI VỊ TRÍ KHỦNG LONG

--- src/levels/LevelConfig.cpp
+++ src/levels/LevelConfig.cpp
@@ -16,8 +16,8 @@ LevelConfig getLevel1()
 
             {ObstacleType::DINOSAUR, 150.f, 1.f, 180.f, "assets/sprites/lv1_sp/dino/dino1.png", 3, 420.f},
             {ObstacleType::DINOSAUR, 200.f, -1.f, 315.f, "assets/sprites/lv1_sp/dino/dino2_trai.png", 3, 430.f},
-            {ObstacleType::MAMMOTH, 90.f, 1.f, 405.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f},
-            {ObstacleType::DINOSAUR, 120.f, -1.f, 270.f, "assets/sprites/lv1_sp/dino/dino3_trai.png", 3, 430.f},
+            {ObstacleType::MAMMOTH, 90.f, 1.f, 270.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f},
+            {ObstacleType::DINOSAUR, 120.f, -1.f, 405.f, "assets/sprites/lv1_sp/dino/dino3_trai.png", 3, 430.f},
             {ObstacleType::MAMMOTH, 80.f, 1.f, 80.f, "assets/sprites/lv1_sp/mammoth/MAMUT.png", 2, 620.f},
         },
         {
